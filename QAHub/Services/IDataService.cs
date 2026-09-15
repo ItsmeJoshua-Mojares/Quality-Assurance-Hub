1448789b46e55f8677dc2537e67d73a3545ec19b
J-Savier <savier.cabello@gmail.com>
2026-09-15 09:32:38 +0800

    Extend IDataService for TestRuns, Requirements, Articles

--- QAHub/Services/IDataService.cs
+++ QAHub/Services/IDataService.cs
@@ -10,8 +10,15 @@ public interface IDataService
     List<Bug> LoadBugs();
     List<Project> LoadProjects();
     List<Shipment> LoadShipments();
+    List<TestRun> LoadTestRuns();
+    List<Requirement> LoadRequirements();
+    List<KnowledgeArticle> LoadKnowledgeArticles();
+
     void SaveTestCases(IEnumerable<TestCase> testCases);
     void SaveBugs(IEnumerable<Bug> bugs);
     void SaveProjects(IEnumerable<Project> projects);
     void SaveShipments(IEnumerable<Shipment> shipments);
+    void SaveTestRuns(IEnumerable<TestRun> testRuns);
+    void SaveRequirements(IEnumerable<Requirement> requirements);
+    void SaveKnowledgeArticles(IEnumerable<KnowledgeArticle> articles);
 }
